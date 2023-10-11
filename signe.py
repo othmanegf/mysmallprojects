f44e77faf60a9090f4235af7601402cfe8f51750
othmanegf <147095181+othmanegf@users.noreply.github.com>
2023-10-11 15:28:53 +0100

    Add files via upload

---
+++ signe.py
@@ -0,0 +1,6 @@
+x = float(input("enter the first number : "))
+y = float(input("enter the second number : "))
+if ( x >= 0 and y >= 0 ) or ( x <= 0 and y <= 0) :
+    print("The two numbers have the same sign")
+else :
+    print("The two numbers have a diffrent sign")
